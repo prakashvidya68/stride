05a523e6275cec52ad461f5249990fb9eaf96ff1
prakashvidya68 <prakashvidya68@gmail.com>
2020-01-03 19:11:29 +0530

    Removing bugs in dashboard

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -56,110 +56,119 @@ class DashBoardScoreBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Container(
       child: Row(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          RotationTransition(
-            turns: new AlwaysStoppedAnimation(270 / 360),
-            child: Text('Stride',
-                style: TextStyle(
-                  fontFamily: 'Audiowide',
-                  fontSize: 25,
-                  color: Color(0xFFF28649),
-                )),
-          ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * 0.3,
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: Card(
-              elevation: 6,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  topLeft: Radius.circular(20),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        dummyUserScore.stepsToday.toString(),
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 50,
-                          color: Color(0xFF5678C1),
-                        ),
-                      ),
-                      Text(
-                        'steps today',
-                        style: TextStyle(
-                            fontFamily: 'Poppins', fontWeight: FontWeight.w100),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Text(
-                            dummyUserScore.kCalBurnt.toString(),
-                            style: TextStyle(
-                                color: Color(0xFFF28649), fontSize: 18),
-                          ),
-                          Text(
-                            ' Kcal ',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                          Text(
-                            dummyUserScore.distWalkedTday.toString(),
-                            style: TextStyle(
-                                color: Color(0xFFF28649), fontSize: 18),
-                          ),
-                          Text(
-                            ' Km’ s.',
-                            style: TextStyle(
-                              fontFamily: 'Poppins',
-                              fontWeight: FontWeight.w300,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Text(
-                        'You have to  walk ${(dummyUserScore.targetDist - dummyUserScore.distWalkedTday).round()} Km’s more',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                      Text(
-                        'Level ${dummyUserScore.currentLevel}',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 22,
-                          color: Color(0xFF5678C1),
-                        ),
-                      ),
-                    ],
+          Flexible(
+              flex: 1,
+              child: RotatedBox(
+                quarterTurns: -1,
+                child: Text('Stride',
+                    style: TextStyle(
+                      fontFamily: 'Audiowide',
+                      height: 2,
+                      fontSize: width * 0.065,
+                      color: Color(0xFFF28649),
+                    )),
+              )),
+          Flexible(
+            flex: 8,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: Card(
+                elevation: 6,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    topLeft: Radius.circular(20),
                   ),
-                  Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(20),
-                          topLeft: Radius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          dummyUserScore.stepsToday.toString(),
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 50,
+                            color: Color(0xFF5678C1),
+                          ),
                         ),
-                      ),
-                      child: Text('GraphHere')
-                      // TODO: IMPLEMENT GRAPH HERE.
-                      ),
-                ],
+                        Text(
+                          'steps today',
+                          style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w100),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Text(
+                              dummyUserScore.kCalBurnt.toString(),
+                              style: TextStyle(
+                                  color: Color(0xFFF28649), fontSize: 18),
+                            ),
+                            Text(
+                              ' Kcal ',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                            Text(
+                              dummyUserScore.distWalkedTday.toString(),
+                              style: TextStyle(
+                                  color: Color(0xFFF28649), fontSize: 18),
+                            ),
+                            Text(
+                              ' Km’ s.',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontWeight: FontWeight.w300,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          'You have to  walk ${(dummyUserScore.targetDist - dummyUserScore.distWalkedTday).round()} Km’s more',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        Text(
+                          'Level ${dummyUserScore.currentLevel}',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 22,
+                            color: Color(0xFF5678C1),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            topLeft: Radius.circular(20),
+                          ),
+                        ),
+                        child: Text('GraphHere')
+                        // TODO: IMPLEMENT GRAPH HERE.
+                        ),
+                  ],
+                ),
               ),
             ),
           ),
